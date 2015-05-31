70f362dd181012de34ea8247facdfbb5fd2b1490
Miriam Moser <mirjoy.moser@gmail.com>
2015-05-31 17:01:27 -0600

    the player can shoot bullets

--- game.js
+++ game.js
@@ -28,6 +28,10 @@
       for (var i = 0; i < this.bodies.length; i++){
         drawRect(screen, this.bodies[i])
       }
+    },
+
+    addBody: function(body){
+      this.bodies.push(body);
     }
   };
 
@@ -45,6 +49,27 @@
       } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)){
         this.center.x += 2;
       }
+
+      if(this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)){
+        var bullet = new Bullet({ x: this.center.x,
+                                  y: this.center.y - this.size.x / 2 },
+                                  { x: 0, y: -6 });
+        this.game.addBody(bullet);
+      }
+    }
+  };
+
+
+  var Bullet = function(center, velocity){
+    this.size = { x: 3, y: 3 };
+    this.center = center;
+    this.velocity = velocity;
+  };
+
+  Bullet.prototype = {
+    update: function(){
+        this.center.x += this.velocity.x;
+        this.center.y += this.velocity.y;
     }
   };
 
