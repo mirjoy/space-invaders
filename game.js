5269cc896e53441b0a02916381c57795405b4e44
Miriam Moser <mirjoy.moser@gmail.com>
2015-05-31 16:27:20 -0600

    added keyboarder

--- game.js
+++ game.js
@@ -48,8 +48,18 @@
     var keyState = {};
 
     window.onkeydown = function(e){
+      keyState[e.keyCode] = true;
+    };
 
-    }
+    window.onkeyup = function(e){
+      keyState[e.keyCode] = false;
+    };
+
+    this.isDown = function(keyCode){
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
   };
 
   window.onload = function(){
